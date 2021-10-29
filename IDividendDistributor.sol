6d39379fe6215137ae57fb598f33f67ccca0bc7e
moongame-io <93387734+moongame-io@users.noreply.github.com>
2021-10-29 20:18:53 +0700

    first commit

---
+++ IDividendDistributor.sol
@@ -0,0 +1,6 @@
+interface IDividendDistributor {
+    function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external;
+    function setShare(address shareholder, uint256 amount) external;
+    function deposit() external payable;
+    function process(uint256 gas) external;
+}
